0bdc071ba84d5d78d40511cf08ca3544a6362655
wansook0316 <wansook0316@gmail.com>
2024-02-15 13:37:32 +0900

    initial project setting.

---
+++ KoalaWatch/KoalaWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoalaWatchApp.swift
+//  KoalaWatch Watch App
+//
+//  Created by Choiwansik on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct KoalaWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
